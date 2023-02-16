16267b6cc9f3dce5481e7ef6bb1aa893ff9c9446
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2023-02-16 23:44:50 +0300

    v1.0.0
    Stable update

--- src/Neural-Network.py
+++ src/Neural-Network.py
@@ -7,15 +7,8 @@ class Neuron():
 		self.delta = 0
 
 class NeuralNetwork:
-	def __init__(self, inputCount, hidenLayers, outputs, learnRate, moment):
+	def __init__(self, inputCount, hidenLayers, outputs, learnRate):
 		self.learnRate = learnRate
-		self.moment = moment
-
-		# Инициализация входного слоя
-		inputs = []
-
-		for i in range(inputCount):
-			inputs.append(Neuron())
 
 		# Инициализация внутрених слоев
 		self.hidenLayers = []
@@ -23,60 +16,37 @@ class NeuralNetwork:
 			self.hidenLayers.append([])
 			for j in range(hidenLayers[i]):
 				self.hidenLayers[i].append(Neuron())
-		
-		# Инициализация выходного слоя
-		output = []
-		for i in range(outputs):
-			output.append(Neuron())
-		
-		# Инициализация весов
-		self.multiArr = [inputs] + self.hidenLayers + [output]
 
+		# Массив нейронов
+		self.multiArr = [[Neuron() for i in range(inputCount)]] + self.hidenLayers + [[Neuron() for _ in range(outputs)]]
+
+		# Инициализация весов
 		self.w = []
 		for i in range(len(self.multiArr) - 1):
 			self.w.append([])
 			for j in range(len(self.multiArr[i])):
 				self.w[i].append([])
 				for k in range(len(self.multiArr[i+1])):
-					self.w[i][j].append(rnd.random()*2-1)
-
-		self.prevW = []
-		for i in range(len(self.multiArr) - 1):
-			self.prevW.append([])
-			for j in range(len(self.multiArr[i])):
-				self.prevW[i].append([])
-				for k in range(len(self.multiArr[i+1])):
-					self.prevW[i][j].append(0)
+					self.w[i][j].append(rnd.random() * 2 - 1)
 	
 	def predict(self, inputs):
 		# Вставка входных данных
-		for i, inp in enumerate(self.multiArr[0]):
-			inp.out = inputs[i]
+		for neurons, inp in zip(self.multiArr[0], inputs):
+			neurons.out = inp
 
-		i = 0
-		# для каждого слоя кроме входного:
-		for i1, layer in enumerate(self.multiArr[1:]):
-			i = i1 + 1
-			# для каждого нейрона слоя:
+		# Расчет суммы и активация
+		for i, layer in enumerate(self.multiArr[1:]):
 			for j, neuron in enumerate(layer):
-				neuron.sum = 0
-				# для каждого нейрона пред.слоя:
-				for k, prevNeuron in enumerate(self.multiArr[i-1]):
-					neuron.sum += prevNeuron.out * self.w[i-1][k][j]
-				neuron.out = self.activation(self.multiArr[i][j].sum)
+				neuron.sum = sum([lastNeuron.out * self.w[i][k][j] for k, lastNeuron in enumerate(self.multiArr[i])])
+				neuron.out = self.activation(neuron.sum)
 
-		res = []
-		for neuron in self.multiArr[-1]:
-			res.append(neuron.out)
-
-		return res
-
-	def activation(self, x):
-		return 1 / (1 + (2 ** (-x)))
+		# Возврат результата
+		return [neuron.out for neuron in self.multiArr[-1]]
 	
 	def learn(self, data, out):
 		res = self.predict(data)
 
+		# Поиск дельт последнего слоя
 		for i, mi in enumerate(self.multiArr[-1]):
 			mi.delta = (out[i] - res[i]) * self.derivativeActivation(res[i])
 		
@@ -93,11 +63,10 @@ class NeuralNetwork:
 			for j, wij in enumerate(wi):
 				for k, wijk in enumerate(wij):
 					deltaW = self.learnRate * self.multiArr[i][j].out * self.multiArr[i+1][k].delta
-					self.w[i][j][k] += deltaW + self.prevW[i][j][k] * self.moment
-					self.prevW[i][j][k] = deltaW
+					self.w[i][j][k] += deltaW
 
-	def exp(self, x):
-		return NeuronNet.e ** x
+	def activation(self, x):
+		return 1 / (1 + (2.7182 ** (-x)))
 
 	def derivativeActivation(self, x):
 		return x * (1 - x)
